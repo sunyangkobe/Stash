de7e76223d1b237ecc87fe4a3c965ba6de7cb6ea
sunyangkobe <yksun@cs.cmu.edu>
2013-04-27 20:54:23 -0400

    basic functionalities are all working, need to polish the UI a little bit

--- Resources/alloy/controllers/map.js
+++ Resources/alloy/controllers/map.js
@@ -1,4 +1,55 @@
 function Controller() {
+    function refreshAnnotations(mapView) {
+        Ti.Geolocation.purpose = "Recieve User Location";
+        Titanium.Geolocation.accuracy = Titanium.Geolocation.ACCURACY_BEST;
+        Titanium.Geolocation.distanceFilter = 10;
+        Titanium.Geolocation.getCurrentPosition(function(e) {
+            if (e.error) {
+                alert("Stash cannot get your current location");
+                return;
+            }
+            mapView.setRegion({
+                latitude: e.coords.latitude,
+                longitude: e.coords.longitude,
+                latitudeDelta: .01,
+                longitudeDelta: .01
+            });
+            getMessagesOnCloud(mapView, e.coords.longitude, e.coords.latitude);
+        });
+    }
+    function getMessagesOnCloud(mapView, lng, lat) {
+        var Cloud = require("ti.cloud");
+        Cloud.debug = true;
+        Cloud.Objects.query({
+            classname: "messages",
+            limit: 50,
+            where: {
+                coordinates: {
+                    $nearSphere: [ lng, lat ],
+                    $maxDistance: .00126
+                }
+            }
+        }, function(e) {
+            e.success ? addAnnotationsOnMap(mapView, e.messages) : alert("Error:\n" + (e.error && e.message || JSON.stringify(e)));
+        });
+    }
+    function addAnnotationsOnMap(mapView, messages) {
+        var annotations = [];
+        for (var i = 0; messages.length > i; i++) {
+            var msg = messages[i];
+            var annotation = Titanium.Map.createAnnotation({
+                latitude: msg.coordinates[0][1],
+                longitude: msg.coordinates[0][0],
+                title: msg.message,
+                subtitle: "created by: " + msg.user.username,
+                image: "/images/marker_purple.png",
+                animate: true,
+                draggable: false
+            });
+            annotations.push(annotation);
+        }
+        mapView.setAnnotations(annotations);
+    }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     arguments[0] ? arguments[0]["__parentSymbol"] : null;
     arguments[0] ? arguments[0]["$model"] : null;
@@ -11,27 +62,15 @@ function Controller() {
     $.__views.mapWin && $.addTopLevelView($.__views.mapWin);
     exports.destroy = function() {};
     _.extend($, $.__views);
-    Ti.Geolocation.purpose = "Recieve User Location";
-    Titanium.Geolocation.accuracy = Titanium.Geolocation.ACCURACY_BEST;
-    Titanium.Geolocation.distanceFilter = 10;
-    Titanium.Geolocation.getCurrentPosition(function(e) {
-        if (e.error) {
-            alert("Stash cannot get your current location");
-            return;
-        }
-        var mapview = Titanium.Map.createView({
-            mapType: Titanium.Map.STANDARD_TYPE,
-            region: {
-                latitude: e.coords.latitude,
-                longitude: e.coords.longitude,
-                latitudeDelta: .01,
-                longitudeDelta: .01
-            },
-            animate: true,
-            regionFit: true,
-            userLocation: true
-        });
-        $.mapWin.add(mapview);
+    var mapview = Titanium.Map.createView({
+        mapType: Titanium.Map.STANDARD_TYPE,
+        animate: true,
+        regionFit: true,
+        userLocation: true
+    });
+    $.mapWin.add(mapview);
+    $.mapWin.addEventListener("focus", function() {
+        refreshAnnotations(mapview);
     });
     _.extend($, exports);
 }
--- app/controllers/map.js
+++ app/controllers/map.js
@@ -16,38 +16,73 @@ if (Ti.Platform.osname == "iphone") {
 	$.mapWin.rightNavButton = postBtn;
 }
 
-Ti.Geolocation.purpose = "Recieve User Location";
-Titanium.Geolocation.accuracy = Titanium.Geolocation.ACCURACY_BEST;
-Titanium.Geolocation.distanceFilter = 10;
-Titanium.Geolocation.getCurrentPosition(function(e) {
-	if (e.error) {
-		alert('Stash cannot get your current location');
-		return;
-	}
+var mapview = Titanium.Map.createView({
+	mapType : Titanium.Map.STANDARD_TYPE,
+	animate : true,
+	regionFit : true,
+	userLocation : true
+});
 
-	// var curLocation = Titanium.Map.createAnnotation({
-	// latitude : e.coords.latitude,
-	// longitude : e.coords.longitude,
-	// title : "My Current Location",
-	// animate : true,
-	// // leftButton : '../images/appcelerator_small.png',
-	// myid : 0 // Custom property to uniquely identify this annotation.
-	// });
+$.mapWin.add(mapview);
+$.mapWin.addEventListener("focus", function(e) {
+	refreshAnnotations(mapview);
+});
 
-	var mapview = Titanium.Map.createView({
-		mapType : Titanium.Map.STANDARD_TYPE,
-		region : {
+function refreshAnnotations(mapView) {
+	Ti.Geolocation.purpose = "Recieve User Location";
+	Titanium.Geolocation.accuracy = Titanium.Geolocation.ACCURACY_BEST;
+	Titanium.Geolocation.distanceFilter = 10;
+	Titanium.Geolocation.getCurrentPosition(function(e) {
+		if (e.error) {
+			alert('Stash cannot get your current location');
+			return;
+		}
+
+		mapView.setRegion({
 			latitude : e.coords.latitude,
 			longitude : e.coords.longitude,
 			latitudeDelta : 0.01,
 			longitudeDelta : 0.01
-		},
-		animate : true,
-		regionFit : true,
-		userLocation : true
-		// annotations : [curLocation]
+		});
+		getMessagesOnCloud(mapView, e.coords.longitude, e.coords.latitude);
 	});
+}
 
-	$.mapWin.add(mapview);
+function getMessagesOnCloud(mapView, lng, lat) {
+	var Cloud = require('ti.cloud');
+	Cloud.debug = true;
+	Cloud.Objects.query({
+		classname : "messages",
+		limit : 50,
+		where : {
+			coordinates : {
+				$nearSphere : [lng, lat],
+				$maxDistance : 0.00126
+			}
+		},
+	}, function(e) {
+		if (e.success) {
+			addAnnotationsOnMap(mapView, e.messages);
+		} else {
+			alert('Error:\n' + ((e.error && e.message) || JSON.stringify(e)));
+		}
+	});
+}
 
-});
+function addAnnotationsOnMap(mapView, messages) {
+	var annotations = [];
+	for (var i = 0; i < messages.length; i++) {
+		var msg = messages[i];
+		var annotation = Titanium.Map.createAnnotation({
+			latitude : msg.coordinates[0][1],
+			longitude : msg.coordinates[0][0],
+			title : msg.message,
+			subtitle : "created by: " + msg.user.username,
+			image : "/images/marker_purple.png",
+			animate : true,
+			draggable : false
+		});
+		annotations.push(annotation);
+	}
+	mapView.setAnnotations(annotations);
+}
